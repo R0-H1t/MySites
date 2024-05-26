900ddc36797227a613190c4bc5429736685c205a
R0-H1t <130152984+R0-H1t@users.noreply.github.com>
2024-05-26 16:49:05 +0530

    Add files via upload
    my first application

---
+++ lyrics.js
@@ -0,0 +1,37 @@
+var _data = {
+    "lyrics": [
+      {"time": 23000, "line": "O sajni re"},
+      {"time": 26000, "line": "Kaise kate din raat"},
+      {"time": 29000, "line": "Kaise ho tujhse baat"},
+      {"time": 32000, "line": "Teri yaad satave re"},
+      {"time": 35000, "line": ""},
+      {"time": 38000, "line": "O sajni re"},
+      {"time": 41000, "line": "Kaise kate din raat"},
+      {"time": 44000, "line": "Kaise mile tera saath"},
+      {"time": 47000, "line": "Teri yaad…"},
+      {"time": 50000, "line": ""},
+      {"time": 53000, "line": "Teri yaad satave re"},
+      {"time": 56000, "line": "Antra"},
+      {"time": 71000, "line": "Kaise ghanere badra ghire"},
+      {"time": 75000, "line": "Teri kami ki barish liye"},
+      {"time": 78000, "line": "Sailaab jo mere seene me hai"},
+      {"time": 81000, "line": "Koi bataye ye kaise thame"},
+      {"time": 84000, "line": "Tere bina ab kaise jiye"},
+      {"time": 87000, "line": ""},
+      {"time": 90000, "line": "O sajni re"},
+      {"time": 92000, "line": "Kaise kate din raat"},
+      {"time": 95000, "line": "Kaise ho tujhse baat"},
+      {"time": 98000, "line": "Teri yaad satave re"},
+      {"time": 101000, "line": "O sajni re"},
+      {"time": 104000, "line": "Kaise kate din raat"},
+      {"time": 107000, "line": "Kaise ho tujhse baat"},
+      {"time": 110000, "line": "Teri yaad…"},
+      {"time": 113000, "line": ""},
+      {"time": 116000, "line": "Teri yaad satave re"},
+      {"time": 119000, "line": "O sajni re"}
+    ]
+  }
+  
+  
+  
+  
